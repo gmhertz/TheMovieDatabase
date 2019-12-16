45e3b24c3700c1975e2af3542bd84607343b6aba
Günter Hertz <gunter.mhertz@gmail.com>
2019-12-15 21:50:54 -0300

    Create project

---
+++ TheMovieDatabase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMovieDatabase
+//
+//  Created by Günter Hertz on 15/12/19.
+//  Copyright © 2019 Günter Hertz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
